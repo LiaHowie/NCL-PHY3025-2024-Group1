adc801c30e36cd60f9ed36d6805da679bd2a6553
LiaHowie <143102480+LiaHowie@users.noreply.github.com>
2024-09-29 16:15:04 +0100

    Linear Least Square Function
    Adding the linear least square method function

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-df_xray = pd.read_csv("SWIFT X-Ray Catalogue.csv", delimiter="|", skiprows=1) # Open X-Ray dataset
+# Open X-Ray dataset
+df_xray = pd.read_csv("SWIFT X-Ray Catalogue.csv", delimiter="|", skiprows=1)
 """ Columns: 
 'BAT_NAME', 'RA', 'DEC', 'SNR', 'COUNTERPART_NAME', 'OTHER_NAME', 'CTPT_RA', 'CTPT_DEC', 'FLUX', 'FLUX_LO',
 'FLUX_HI', 'CONTA', 'GAMMA', 'GAMM_LO', 'GAMM_HI', 'CHI_SQ_R', 'REDSHIFT', 'LUM', 'ASSOC_STREN', 'CL2', 'TYPE'
 """
 print(df_xray)
-print(list(df_xray))
 print(df_xray.loc[1, 'RA'])
+print(df_xray['RA'])
 
-df_ir = pd.read_csv("table_irsa_catalog_search_results.csv", delimiter=",", skiprows=0) # Open Infrared dataset
+# Open Infrared dataset
+df_ir = pd.read_csv("table_irsa_catalog_search_results.csv", delimiter=",", skiprows=0)
 """ Columns:
 galname', 'rah', 'ram', 'ras', 'decsign', 'decd', 'decm', 'decs', 'glat_orig', 'semimajor', 'semiminor', 
 'posang', 'nhcon', 'fnu_12', 'fqual_12', 'fnu_25', 'fqual_25', 'fnu_60', 'fqual_60', 'fnu_100', 'fqual_100', 
@@ -21,7 +25,53 @@ galname', 'rah', 'ram', 'ras', 'decsign', 'decd', 'decm', 'decs', 'glat_orig', '
 'cdec', 'ra1950', 'dec1950', 'cra1950', 'cdec1950', 'ROW_IDX', 'ROW_NUM'
 """
 print(df_ir)
-print(list(df_ir))
 print(df_ir.loc[1, 'ra'])
+print(df_ir['ra'])
 
-# Hi
+
+def lin_lstsq(x,y):
+    # Function for a linear least square fit -- Lia
+    # maths for a and b provided by Jason
+    N = len(x)
+    a = (np.sum(x*y)-(1/N)*np.sum(x)*np.sum(y))/(np.sum(x**2)-(1/N)*(np.sum(x)**2))
+    b = np.mean(y) - a*np.mean(x)
+    return [a,b]
+
+
+
+
+
+
+
+
+
+
+
+
+# Test Data:
+data = []
+for i in range(1,6):
+    data.append(i)
+data = np.array(data,dtype="float64")
+x = data
+y = data*2
+
+""" x = np.array([2,3,3,3,4,4,5,5,5,6])
+y = np.array([28.7,24.8,26.0,30.5,23.8,24.6,23.8,20.4,21.6,22.1]) """
+fit = lin_lstsq(x,y)
+print(np.sum(x))
+print(np.sum(y))
+print(np.sum(x**2))
+print(np.sum(y**2))
+print(np.sum(x*y))
+
+print(fit)
+
+plt.figure()
+plt.scatter(x,y)
+
+fitx = np.linspace(0,np.max(x))
+fity = fit[0]*fitx + fit[1]
+plt.plot(fitx,fity)
+
+plt.show()
