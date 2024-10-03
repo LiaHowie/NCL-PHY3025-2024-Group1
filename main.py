796134028e3980dd6c9fa8d713190d6c77b6a2ac
LiaHowie <143102480+LiaHowie@users.noreply.github.com>
2024-10-03 18:39:31 +0100

    IRAS dataset correction
    Swapping to the actual dataset as we originally had the wrong one

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ print(df_xray['RA'])
 # Open Infrared dataset
 df_ir = pd.read_csv("table_irsa_catalog_search_results.csv", delimiter=",", skiprows=0)
 """ Columns:
-galname', 'rah', 'ram', 'ras', 'decsign', 'decd', 'decm', 'decs', 'glat_orig', 'semimajor', 'semiminor', 
-'posang', 'nhcon', 'fnu_12', 'fqual_12', 'fnu_25', 'fqual_25', 'fnu_60', 'fqual_60', 'fnu_100', 'fqual_100', 
-'fir', 'fqfir', 'relunc_12', 'relunc_25', 'relunc_60', 'relunc_100', 'cc_12', 'cc_25', 'cc_60', 'cc_100', 
-'cirr1', 'cirr2', 'confuse', 'pnearh', 'pnearw', 'hsdflag', 'ses1_12', 'ses1_25', 'ses1_60', 'ses1_100', 
-'ses2', 'nsss', 'sssname', 'dissss', 'nongal', 'idngal', 'dsngal', 'nrecs', 'rat_12_25', 'err_12_25', 
-'rat_25_60', 'err_25_60', 'rat_60_100', 'err_60_100', 'glon', 'glat', 'elon', 'elat', 'ra', 'dec', 'cra', 
-'cdec', 'ra1950', 'dec1950', 'cra1950', 'cdec1950', 'ROW_IDX', 'ROW_NUM'
+'pscname', 'rah', 'ram', 'ras', 'decsign', 'decd', 'decm', 'decs', 'semimajor', 'semiminor', 'posang', 'nhcon', 'fnu_12',
+'fnu_25', 'fnu_60', 'fnu_100', 'fqual_12', 'fqual_25', 'fqual_60', 'fqual_100', 'nlrs', 'lrschar', 'relunc_12', 'relunc_25',
+'relunc_60', 'relunc_100', 'tsnr_12', 'tsnr_25', 'tsnr_60', 'tsnr_100', 'cc_12', 'cc_25', 'cc_60', 'cc_100', 'var', 'disc',
+'confuse', 'pnearh', 'pnearw', 'ses1_12', 'ses1_25', 'ses1_60', 'ses1_100', 'ses2_12', 'ses2_25', 'ses2_60', 'ses2_100',
+'hsdflag', 'cirr1', 'cirr2', 'cirr3', 'nid', 'idtype', 'mhcon', 'fcor_12', 'fcor_25', 'fcor_60', 'fcor_100', 'rat_12_25',
+'err_12_25', 'rat_25_60', 'err_25_60', 'rat_60_100', 'err_60_100', 'glon', 'glat', 'elon', 'elat', 'ra', 'dec', 'cra',
+'cdec', 'ra1950', 'dec1950', 'cra1950', 'cdec1950'
 """
 print(df_ir)
 print(df_ir.loc[1, 'ra'])
