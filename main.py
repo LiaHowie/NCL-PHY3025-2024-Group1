14f7a956fd4a5e3986b9dc32436b7a2c8160584b
LiaHowie <143102480+LiaHowie@users.noreply.github.com>
2024-11-01 19:31:08 +0000

    R^2 Calculations
    Added R^2 calculations for our best fit lines. Additionally added extra plotting such as a non-logged SFR vs Lagn and more attempts at 3D best fits

--- main.py
+++ main.py
@@ -50,11 +50,13 @@ def nonlinpoly_lstsq(x,y,m):
     # where y = a[m]*x**m + a[m-1]*x**(m-1) + ... + a[1]*x + a[0]
     return a
 
-def sim_solve_3d(a,b,c,d,e,f):
-    a1 = (1/(e - a*c))
-    a2 = (-a/(e - a*c))
-    a3 =(a*d - f)/(e - a*c) 
-    return [a1,a2,a3]
+
+def R2(y, yfit):
+    # R^2 method for testing how good of a fit our functions are
+    r = y - yfit
+    a = y - np.mean(y)
+    r2 = 1- ((np.sum(r**2))/(np.sum(a**2)))
+    return r2
 
 
 def lum(f,d):
@@ -121,7 +123,7 @@ mpl.rcParams['font.size'] = 14
 # Defining custom colour gradient for redshift
 clrs = mpl.colors.LinearSegmentedColormap.from_list("custom", ["orange","red","firebrick"])
 
-# Defining length of dummy arrays
+# Defining length of dummy arrays to be used when plotting best fit lines, simply ensures they are all the same length index-wise
 dmy_len = 1000000
 
 
@@ -153,6 +155,8 @@ plt.xlabel("$\log($AGN X-Ray Luminosity, $L_{\odot})$")
 plt.ylabel("$\log($SFR, $M_{\odot}\cdot$yr$^{-1})$")
 plt.legend()
 
+print(f"R^2 = {R2(y=log_sfr, yfit=fit_sfr_Lagn[0]*log_lum_xray + fit_sfr_Lagn[1])}")
+
 
 #%%
 # Create figure - log(lum_xray) vs log(z)
@@ -182,6 +186,7 @@ plt.xlabel("$\log($Redshift$)$")
 plt.ylabel("$\log($AGN X-Ray Luminosity, $L_{\odot})$")
 plt.legend(loc="lower right")
 
+print(f"R^2 = {R2(y=log_lum_xray, yfit=fit_Lagn_z[0]*log_z + fit_Lagn_z[1])}")
 
 
 #%%
@@ -212,12 +217,70 @@ plt.xlabel("$\log($Redshift$)$")
 plt.ylabel("$\log($SFR, $M_{\odot}\cdot$yr$^{-1})$")
 plt.legend(loc="lower right")
 
+print(f"R^2 = {R2(y=log_sfr, yfit=fit_sfr_z[0]*log_z + fit_sfr_z[1])}")
+
+
+#%%
+# Create figure - SFR vs lum_xray
+fig, ax = plt.subplots()
+
+# Plot scatter plot of lum_xray, sfr with redshift determining point colour
+scatter = ax.scatter(lum_xray, sfr, marker='x', label="Data", c=z, cmap=clrs)
+
+# Plot a colourbar to show how the datapoint colour changes depending on redshift
+clrbar = plt.colorbar(scatter)
+clrbar.set_label("Redshift")
+
+# Create an array of dummy x-axis values
+fitx = np.linspace(np.min(lum_xray),np.max(lum_xray),dmy_len)
+# Calculate the the best fit line using the linear least square outputted parameters and the continuous value of x=log(lum_xray)
+fity_sfr_Lagn_lin = (fitx**(fit_sfr_Lagn[0]))*(10**(fit_sfr_Lagn[1]))
+
+# Plot the best fit line
+ax.plot(fitx,fity_sfr_Lagn_lin, color='firebrick', linewidth=1, label="Fit Line")
+
+# Label axes and plot legend
+plt.xlabel("AGN X-Ray Luminosity, $L_{\odot}$")
+plt.ylabel("SFR, $M_{\odot}\cdot$yr$^{-1}$")
+plt.legend()
+
+# Create figure - SFR vs lum_xray (y-limited)
+fig, ax = plt.subplots()
+
+# Plot scatter plot of lum_xray, sfr with redshift determining point colour
+scatter = ax.scatter(lum_xray, sfr, marker='x', label="Data", c=z, cmap=clrs)
+
+# Plot a colourbar to show how the datapoint colour changes depending on redshift
+clrbar = plt.colorbar(scatter)
+clrbar.set_label("Redshift")
+
+# Create an array of dummy x-axis values
+fitx = np.linspace(np.min(lum_xray),np.max(lum_xray),dmy_len)
+# Calculate the the best fit line using the linear least square outputted parameters and the continuous value of x=log(lum_xray)
+fity_sfr_Lagn_lin = (fitx**(fit_sfr_Lagn[0]))*(10**(fit_sfr_Lagn[1]))
+
+# Plot the best fit line
+ax.plot(fitx,fity_sfr_Lagn_lin, color='firebrick', linewidth=1, label="Fit Line")
+
+# Label axes and plot legend
+plt.xlabel("AGN X-Ray Luminosity, $L_{\odot}$")
+plt.ylabel("SFR, $M_{\odot}\cdot$yr$^{-1}$")
+plt.ylim(0,36)
+plt.legend()
+
+
 
 #%%
 
 """ 
-NOTE: This section is unfinished
+NOTE: This section is useless for the actual project but whatever
+
+To fix our 3D stuff do Gradient Descent Method
 """
+print("===================================================")
+
+plt.rcParams['grid.color'] = (0.5, 0.5, 0.5, 0.2)
+
 # Create figure - 3D plot of all three variables
 fig = plt.figure()
 ax = fig.add_subplot(projection="3d")
@@ -229,24 +292,82 @@ scatter = ax.scatter(log_lum_xray, log_sfr, log_z, marker='x', label="Data", c=z
 clrbar = plt.colorbar(scatter,location='left')
 clrbar.set_label("Redshift")
 
-# Find the 3D best fit line equation using the parameters from the three 2D best fit lines
-fit = sim_solve_3d(a=fit_sfr_Lagn[0], b=fit_sfr_Lagn[1], c=fit_Lagn_z[0], d=fit_Lagn_z[1], e=fit_sfr_z[0], f=fit_sfr_z[1])
-# Create an array of dummy x-axis and z-axis values
-fitx = np.linspace(min(log_sfr),max(log_sfr),dmy_len)
-fitz = np.linspace(min(log_z),max(log_z),dmy_len)
-# Calculate the the best fit line using the linear least square outputted parameters and the continuous value of x=log(z)
-fity = (fity_sfr_Lagn + fity_sfr_z + fity_Lagn_z)/3
-# Print the best fit equation to console
+# Dummy arrays for plotting continuous line
+fitx = np.linspace(min(log_lum_xray),max(log_lum_xray),dmy_len)
+fity = np.linspace(min(log_sfr),max(log_sfr),dmy_len)
 
-# Plot the best fit line
-ax.plot(fitx, fity, fitz, color='firebrick', linewidth=1, label="Fit Line")
+# This is the LS line of best fit estimated ustilising http://dx.doi.org/10.18642/ijamml_7100121818
+a = fit_Lagn_z[0]
+b = fit_Lagn_z[1]
+c = fit_sfr_z[0]
+d = fit_sfr_z[1]
+x0 = np.mean(log_lum_xray)
+y0 = np.mean(log_sfr)
+z0 = np.mean(log_z)
+fitz = ((fitx - x0)/(2*a)) + ((fity - y0)/(2*c)) + z0
+# Plot the best LS fit line
+ax.plot(fitx, fity, fitz, color='purple', linewidth=1, label="LS Fit Line")
+# Print the best fit equation
+print(f"log(z) = (log(lum_xray)-{x0})/({2*a}) + (log(SFR)-{y0})/({2*c}) + {z0}")
+
+# Here is our modified version of the above fit:
+def sim_solve_3d(a,b,c,d,e,f):
+    a1 = (1/(e - a*c))
+    a2 = (-a/(e - a*c))
+    a3 =(a*d - f)/(e - a*c) 
+    return [a1,a2,a3]
+
+fit = sim_solve_3d(a=a, b=b, c=c, d=d, e=fit_sfr_Lagn[0], f=fit_sfr_Lagn[1])
+fitz1 = ((fitx - x0)*fit[0]) + ((fity - y0)*fit[1]) + z0 #+ fit[2]
+# Plot the modified LS fit
+ax.plot(fitx, fity, fitz1, color='blue', linewidth=1, label="Modified LS Fit Line")
+# Print the best fit equation
+print(f"log(z) = (log(lum_xray)-{x0})*({fit[0]}) + (log(SFR)-{y0})*({fit[1]}) + {z0} (+ {fit[2]} --> we removed this factor)")
+
+
+"""
+# This is code for plotting the computers best estimate for a line of best fit
+# This plot is so bad I'm keeping it comented forever
+from skspatial.objects import Line, Points
+
+points_array = []
+for i in range(len(log_lum_xray)):
+    points_array.append([log_lum_xray[i],log_sfr[i],log_z[i]])
+
+points = Points(points_array)
+
+line_fit = Line.best_fit(points)
+points_projected = line_fit.project_points(points)
+points_projected.plot_2d(ax, color='green', s=50, zorder=3) """
+
+# The next 2 comments worth of code is to draw a line between the two extremes of the scatter plot - just for funzies
+# Create a matrix of coordinates of the datapoints in 3d space
+pts = []
+for i in range(len(log_lum_xray)):
+    pts.append([log_lum_xray[i],log_sfr[i],log_z[i]])
+pts = np.array(pts)
+# Find the indices of the 2 points with the largest distance between them
+max_dist = [0,0,0]
+max_dist_idx = []
+for i in range(len(pts)):
+    for j in range(len(pts)):
+        if abs(pts[i][0]-pts[j][0]) > abs(max_dist[0]) and abs(pts[i][1]-pts[j][1]) > abs(max_dist[1]) and abs(pts[i][2]-pts[j][2]) > abs(max_dist[2]):
+            max_dist = pts[i]-pts[j]
+            max_dist_idx = [i, j]
+i = max_dist_idx[0]
+j = max_dist_idx[1]
+# Drawing a line between the two extreme points using these indices
+ax.plot([log_lum_xray[i],log_lum_xray[j]],[log_sfr[i],log_sfr[j]],[log_z[i],log_z[j]], linestyle='--', color='green', label='Line between extremes')
+
+
+# Plotting the centra point of the point cloud
+ax.scatter(x0,y0,z0, color="green", label="Point Cloud Origin")
 
 # Label axes and plot legend
 ax.set_xlabel("$\log($AGN X-Ray Luminosity, $L_{\odot})$", labelpad=10)
 ax.set_ylabel("$\log($SFR, $M_{\odot}\cdot$yr$^{-1})$", labelpad=10)
 ax.set_zlabel("$\log($Redshift$)$", labelpad=10)
 plt.legend()
-
 
 
 
